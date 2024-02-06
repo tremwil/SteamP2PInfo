873237f381520b6d47bc19a1ba588035e43a5f30
William Tremblay <tremwil@gmail.com>
2024-02-06 01:18:20 -0500

    Bump version

--- SteamP2PInfo/VersionCheck.cs
+++ SteamP2PInfo/VersionCheck.cs
@@ -14,7 +14,7 @@ namespace SteamP2PInfo
 {
     static class VersionCheck
     {
-        public static readonly string CurrentVersion = "V1.1.0";
+        public static readonly string CurrentVersion = "V1.2.0-pre1";
         public static JObject LatestRelease { get; private set; }
 
         public static bool FetchLatest()
